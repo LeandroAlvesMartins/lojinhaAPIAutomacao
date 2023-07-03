0e663a3d7d2b3a857b6baf7356ff112fab091cc3
Leandro Martins <leandro.2204@icloud.com>
2023-07-03 20:38:58 -0300

    Alterei a basePath para lojinha-bugada

--- src/test/java/modulos/produto/ProdutoTest.java
+++ src/test/java/modulos/produto/ProdutoTest.java
@@ -26,7 +26,7 @@ public class ProdutoTest {
     public void beforeEach(){
         // Configurando os dados API Rest da Lojinha
         baseURI = "http://165.227.93.41";
-        basePath = "/lojinha";
+        basePath = "/lojinha-bugada";
 
         //Obter o Token do usuário Admin
         this.token = given()
